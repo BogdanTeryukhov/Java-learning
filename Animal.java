7eb4111ede1e29ce7785a4491fa5b158ec3f271d
BogdanTeryukhov <andrejteryukhov5@gmail.com>
2022-07-30 20:20:28 +0300

    added Polymorfism example

---
+++ Animal.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.List;
+
+abstract class Animal {
+    private int age;
+    private String name;
+
+    public Animal(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public void speak(){
+        System.out.println("None");
+    }
+}
+
+class Dog extends Animal{
+    String breed;
+
+    public Dog(int age, String name, String breed) {
+        super(age, name);
+        this.breed = breed;
+    }
+
+    @Override
+    public void speak(){
+        System.out.println("Waf Waf");
+    }
+}
+
+class Cat extends Animal{
+    String breed;
+
+    public Cat(int age, String name, String breed) {
+        super(age, name);
+        this.breed = breed;
+    }
+
+    @Override
+    public void speak(){
+        System.out.println("Meow");
+    }
+}
+
+class Test{
+    public static void main(String[] args) {
+        Animal dog = new Dog(12, "George", "Haski");
+        Animal cat = new Cat(8,"Tom", "Ji");
+
+        List<Animal> pets = Arrays.asList(dog,cat);
+
+        for (Animal d: pets) {
+            d.speak();
+        }
+    }
+}
+
